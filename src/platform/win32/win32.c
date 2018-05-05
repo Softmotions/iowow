8a04d3249a5a81be7ffea4d314ea910a6ac0b95f
Anton Adamansky <adamansky@gmail.com>
2018-05-05 18:49:43 +0700

    #1

--- src/platform/win32/win32.c
+++ src/platform/win32/win32.c
@@ -205,7 +205,6 @@ iwrc iwp_pread(HANDLE fh, off_t off, void *buf, size_t siz, size_t *sp) {
   return 0;
 }
 
-
 iwrc iwp_pwrite(HANDLE fh, off_t off, const void *buf, size_t siz, size_t *sp) {
   if (INVALIDHANDLE(fh)) {
     return IW_ERROR_INVALID_HANDLE;
@@ -260,35 +259,3 @@ iwrc iwp_lseek(HANDLE fh, off_t offset, iwp_seek_origin origin, off_t *pos) {
   }
   return 0;
 }
-
-#if !defined(IW_NODLL) && defined(PTW32_VERSION)
-BOOL WINAPI DllMain(HINSTANCE hinstDll, DWORD fdwReason, LPVOID lpvReserved) {
-  BOOL result = PTW32_TRUE;
-  switch (fdwReason) {
-    case DLL_PROCESS_ATTACH:
-      result = pthread_win32_process_attach_np();
-      break;
-      
-    case DLL_THREAD_ATTACH:
-      /*
-       * A thread is being created
-       */
-      result = pthread_win32_thread_attach_np();
-      break;
-      
-    case DLL_THREAD_DETACH:
-      /*
-       * A thread is exiting cleanly
-       */
-      result = pthread_win32_thread_detach_np();
-      break;
-      
-    case DLL_PROCESS_DETACH:
-      (void) pthread_win32_thread_detach_np();
-      result = pthread_win32_process_detach_np();
-      break;
-  }
-  return (result);
-  
-} /* DllMain */
-#endif
