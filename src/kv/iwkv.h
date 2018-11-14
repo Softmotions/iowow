ac234acfea4585fba8acdfac4492c2a7c74b049a
Anton Adamansky <adamansky@gmail.com>
2018-11-15 01:24:25 +0700

    - Removed unused fields from IWKV_OPTS (iwkv.h)

--- src/kv/iwkv.h
+++ src/kv/iwkv.h
@@ -143,9 +143,6 @@ typedef struct IWKV_OPTS {
   const char *path;                 /**< Path to database file */
   int32_t random_seed;              /**< Random seed used for iwu random generator */
   iwkv_openflags oflags;            /**< Bitmask of database file open modes */
-  size_t wal_buffer_sz;             /**< Size of in-memory WAL file buffer */
-  size_t wal_checkpoint_after_sz;   /**< Perform checkpoint after specified number of bytes changed */
-  uint32_t wal_checkpoint_every_ms; /**< Perform checkpoint after period of time specified as milliseconds */
   IWKV_WAL_OPTS wal;                /**< WAL options */
 } IWKV_OPTS;
 
