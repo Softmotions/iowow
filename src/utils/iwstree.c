7da571040909bc87190ee04922839dc83e75ef58
Anton Adamansky <adamansky@gmail.com>
2020-02-05 18:52:36 +0700

    * Added splay tree implementation

---
+++ src/utils/iwstree.c
@@ -0,0 +1,263 @@
+/*
+Copyright (c) 2011, Willem-Hendrik Thiart
+Copyright (c) 2012-2020 Softmotions Ltd <info@softmotions.com>
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * The names of its contributors may not be used to endorse or promote
+      products derived from this software without specific prior written
+      permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL WILLEM-HENDRIK THIART BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include "iwstree.h"
+#include "iwlog.h"
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <errno.h>
+
+#define LEFT 0
+#define RIGHT 1
+
+typedef struct _tree_node_t {
+  struct _tree_node_t  *left, *right;
+  void *key, *value;
+} tree_node_t;
+
+IWSTREE *iwstree_create(
+  int (*cmp)(const void *, const void *)
+) {
+  IWSTREE *st;
+  st = malloc(sizeof(IWSTREE));
+  if (!st) {
+    return 0;
+  }
+  memset(st, 0, sizeof(IWSTREE));
+  st->cmp = cmp;
+  return st;
+}
+
+static void _free_node(tree_node_t *node) {
+  if (node) {
+    _free_node(node->left);
+    _free_node(node->right);
+    free(node);
+  }
+}
+
+void iwstree_free(IWSTREE *st) {
+  _free_node(st->root);
+  free(st);
+}
+
+static tree_node_t *_init_node(void *key, void *value) {
+  tree_node_t *n;
+  n = malloc(sizeof(tree_node_t));
+  if (!n) {
+    return 0;
+  }
+  n->left = n->right = 0;
+  n->key = key;
+  n->value = value;
+  return n;
+}
+
+static void _rotate_right(tree_node_t **pa) {
+  tree_node_t *child;
+  child = (*pa)->left;
+  assert(child);
+  (*pa)->left = child->right;
+  child->right = *pa;
+  *pa = child;
+}
+
+static void _rotate_left(tree_node_t **pa) {
+  tree_node_t *child;
+  child = (*pa)->right;
+  assert(child);
+  (*pa)->right = child->left;
+  child->left = *pa;
+  *pa = child;
+}
+
+/**
+ * bring this value to the top
+ * */
+static tree_node_t *_splay(
+  IWSTREE *st,
+  int update_if_not_found,
+  tree_node_t **gpa,
+  tree_node_t **pa,
+  tree_node_t **child,
+  const void *key) {
+
+  int cmp;
+  tree_node_t *next;
+
+  if (!(*child)) {
+    return 0;
+  }
+  cmp = st->cmp((*child)->key, key);
+  if (cmp == 0) {
+    next = *child;
+  } else if (cmp > 0) {
+    next = _splay(st, update_if_not_found, pa, child, &(*child)->left, key);
+  } else {
+    next = _splay(st, update_if_not_found, pa, child, &(*child)->right, key);
+  }
+  if (!next) {
+    if (update_if_not_found) {
+      next = *child;
+    } else {
+      return 0;
+    }
+  } else {
+    if (next != *child)
+      return next;
+  }
+
+  if (!pa)
+    return next;
+
+  if (!gpa) {
+    /* zig left */
+    if ((*pa)->left == next) {
+      _rotate_right(pa);
+    }
+    /* zig right */
+    else {
+      _rotate_left(pa);
+    }
+    return next;
+  }
+
+  assert(gpa);
+
+  /* zig zig left */
+  if ((*pa)->left == next && (*gpa)->left == *pa) {
+    _rotate_right(pa);
+    _rotate_right(gpa);
+  }
+  /* zig zig right */
+  else if ((*pa)->right == next && (*gpa)->right == *pa) {
+    _rotate_left(pa);
+    _rotate_left(gpa);
+  }
+  /* zig zag right */
+  else if ((*pa)->right == next && (*gpa)->left == *pa) {
+    _rotate_left(pa);
+    _rotate_right(gpa);
+  }
+  /* zig zag left */
+  else if ((*pa)->left == next && (*gpa)->right == *pa) {
+    _rotate_right(pa);
+    _rotate_left(gpa);
+  }
+  return next;
+}
+
+int iwstree_is_empty(IWSTREE *st) {
+  return st->root == 0;
+}
+
+void *iwstree_remove(IWSTREE *st, const void *key) {
+  tree_node_t *root, *left_highest;
+  void *val;
+
+  /*  make removed node the root */
+  if (!iwstree_get(st, key)) {
+    return 0;
+  }
+  root = st->root;
+  val = root->value;
+
+  assert(0 < st->count);
+  assert(root->key == key);
+
+  /* get left side's most higest value node */
+  if ((left_highest = root->left)) {
+    tree_node_t *prev = root;
+    while (left_highest->right) {
+      prev = left_highest;
+      left_highest = left_highest->right;
+    }
+    /* do the swap */
+    prev->right = 0;
+    st->root = left_highest;
+    left_highest->left = root->left;
+    left_highest->right = root->right;
+  } else {
+    assert(root);
+    st->root = root->right;
+  }
+  st->count--;
+  assert(root != st->root);
+  free(root);
+  return val;
+}
+
+/**
+ * get this item referred to by key. Slap it as root.
+ */
+void *iwstree_get(IWSTREE *st, const void *key) {
+  tree_node_t *node;
+  node = _splay(st, 0, 0, 0, (tree_node_t **) &st->root, key);
+  return node ? node->value : 0;
+}
+
+int iwstree_count(IWSTREE *st) {
+  return st->count;
+}
+
+void *iwstree_peek(IWSTREE *st) {
+  return st->root ? ((tree_node_t *) st->root)->value : 0;
+}
+
+iwrc iwstree_put(IWSTREE *st, void *key, void *value) {
+  tree_node_t *new;
+  int cmp;
+  if (!st->root) {
+    st->root = _init_node(key, value);
+    if (!st->root) {
+      return iwrc_set_errno(IW_ERROR_ALLOC, errno);
+    }
+    st->count++;
+    goto exit;
+  }
+  new = _splay(st, 1, 0, 0, (tree_node_t **) &st->root, key);
+  cmp = st->cmp(((tree_node_t *) st->root)->key, key);
+  if (cmp != 0) {
+    new = _init_node(key, value);
+    if (0 < cmp) {
+      new->right = st->root;
+      new->left = new->right->left;
+      new->right->left = 0;
+    } else {
+      new->left = st->root;
+      new->right = new->left->right;
+      new->left->right = 0;
+    }
+    st->count++;
+  }
+  st->root = new;
+
+exit:
+  return 0;
+}
---
+++ src/utils/iwstree.h
@@ -0,0 +1,65 @@
+#pragma once
+#ifndef IWSTREE_H
+#define IWSTREE_H
+
+/*
+Copyright (c) 2011, Willem-Hendrik Thiart
+Copyright (c) 2012-2020 Softmotions Ltd <info@softmotions.com>
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * The names of its contributors may not be used to endorse or promote
+      products derived from this software without specific prior written
+      permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL WILLEM-HENDRIK THIART BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include "basedefs.h"
+
+IW_EXTERN_C_START
+
+#include "basedefs.h"
+
+typedef struct {
+  void *root;
+  int (*cmp)(const void *, const void *);
+  int count;
+} IWSTREE;
+
+IWSTREE *iwstree_create(
+  int (*cmp)(const void *,
+             const void *)
+);
+
+IW_EXPORT void iwstree_free(IWSTREE *st);
+
+IW_EXPORT int iwstree_is_empty(IWSTREE *st);
+
+IW_EXPORT void *iwstree_remove(IWSTREE *st, const void *key);
+
+IW_EXPORT void *iwstree_get(IWSTREE *st, const void *key);
+
+IW_EXPORT int iwstree_count(IWSTREE *st);
+
+IW_EXPORT void *iwstree_peek(IWSTREE *st);
+
+IW_EXPORT iwrc iwstree_put(IWSTREE *st, void *key, void *value);
+
+IW_EXTERN_C_END
+#endif
