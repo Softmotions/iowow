a63ebcb41986d3c8d63feb16df619fd5e5e6f7cd
Anton Adamansky <adamansky@gmail.com>
2021-08-13 13:53:23 +0700

    * Typo fixed

--- src/basedefs.h
+++ src/basedefs.h
@@ -162,8 +162,10 @@ typedef int HANDLE;
 #ifndef RCT
 #define RCT(label__, val__)                                         \
   ({ __typeof__(val__) v__ = (val__);                               \
-     if (v__) rc = iwrc_set_errno(IW_ERROR_THREADING_ERRNO, v__);  \
-     goto label__;                                                  \
+     if (v__) {                                                     \
+       rc = iwrc_set_errno(IW_ERROR_THREADING_ERRNO, v__);          \
+       goto label__;                                                \
+     }                                                              \
    })
 #endif
 
