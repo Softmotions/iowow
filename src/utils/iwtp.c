7a21626ec037a0ff1a1201ab456cca8075bfca88
Anton Adamansky <adamansky@gmail.com>
2022-08-24 17:32:05 +0700

    * overflow threads support (iwtp.h)

--- src/utils/iwtp.c
+++ src/utils/iwtp.c
@@ -2,6 +2,7 @@
 #include "iwth.h"
 #include "iwp.h"
 #include "iwlog.h"
+#include "iwarr.h"
 
 #include <stdlib.h>
 #include <errno.h>
@@ -19,13 +20,20 @@ struct iwtp {
   struct task    *tail;
   pthread_mutex_t mtx;
   pthread_cond_t  cond;
-  pthread_t      *threads;
+  IWULIST threads;
+
   char *thread_name_prefix;
   int   num_threads;
+  int   num_threads_busy;
+  int   overflow_threads_factor;
   int   queue_limit;
-  int   cnt;
+  int   queue_size;
+
+  bool warn_on_overflow_thread_spawn;
   volatile bool shutdown;
 };
+
+static void* _worker_fn(void *op);
 
 iwrc iwtp_schedule(IWTP tp, iwtp_task_f fn, void *arg) {
   if (!tp || !fn) {
@@ -44,17 +52,14 @@ iwrc iwtp_schedule(IWTP tp, iwtp_task_f fn, void *arg) {
     .arg = arg
   };
 
-  int rci = pthread_mutex_lock(&tp->mtx);
-  if (rci) {
-    rc = iwrc_set_errno(IW_ERROR_THREADING_ERRNO, errno);
-    goto finish;
-  }
+  pthread_mutex_lock(&tp->mtx);
   if (tp->shutdown) {
     rc = IW_ERROR_INVALID_STATE;
     pthread_mutex_unlock(&tp->mtx);
     goto finish;
   }
-  if (tp->queue_limit && (tp->cnt + 1 > tp->queue_limit)) {
+
+  if (tp->queue_limit && (tp->queue_size + 1 > tp->queue_limit)) {
     rc = IW_ERROR_OVERFLOW;
     pthread_mutex_unlock(&tp->mtx);
     goto finish;
@@ -66,7 +71,14 @@ iwrc iwtp_schedule(IWTP tp, iwtp_task_f fn, void *arg) {
     tp->head = task;
     tp->tail = task;
   }
-  ++tp->cnt;
+  ++tp->queue_size;
+
+  if (  tp->num_threads_busy >= tp->num_threads
+     && iwulist_length(&tp->threads) < tp->num_threads * (1 + tp->overflow_threads_factor)) {
+    pthread_t th;
+    pthread_create(&th, 0, _worker_fn, tp);
+  }
+
   pthread_cond_signal(&tp->cond);
   pthread_mutex_unlock(&tp->mtx);
 
@@ -81,16 +93,28 @@ static void* _worker_fn(void *op) {
   struct iwtp *tp = op;
   assert(tp);
 
+  pthread_t st = pthread_self();
+
+  pthread_mutex_lock(&tp->mtx);
+  size_t idx = iwulist_length(&tp->threads);
+  if (iwulist_push(&tp->threads, &st)) {
+    pthread_mutex_unlock(&tp->mtx);
+    return 0;
+  }
   if (tp->thread_name_prefix) {
-    pthread_t st = pthread_self();
-    for (int i = 0; i < tp->num_threads; ++i) {
-      if (tp->threads[i] == st) {
-        char nbuf[strlen(tp->thread_name_prefix) + 16];
-        snprintf(nbuf, sizeof(nbuf), "%s%d", tp->thread_name_prefix, i);
-        iwp_set_current_thread_name(nbuf);
-        break;
-      }
+    char nbuf[64];
+    if (idx >= tp->num_threads) {
+      snprintf(nbuf, sizeof(nbuf), "%s%zd+", tp->thread_name_prefix, idx);
+    } else {
+      snprintf(nbuf, sizeof(nbuf), "%s%zd", tp->thread_name_prefix, idx);
     }
+    iwp_set_current_thread_name(nbuf);
+  }
+  pthread_mutex_unlock(&tp->mtx);
+
+  if (tp->warn_on_overflow_thread_spawn && idx >= tp->num_threads) {
+    iwlog_warn("iwtp | Overflow thread spawned: %s%zd+",
+               tp->thread_name_prefix ? tp->thread_name_prefix : "", idx);
   }
 
   while (true) {
@@ -98,6 +122,7 @@ static void* _worker_fn(void *op) {
     iwtp_task_f fn = 0;
 
     pthread_mutex_lock(&tp->mtx);
+    ++tp->num_threads_busy;
     if (tp->head) {
       struct task *h = tp->head;
       fn = h->fn;
@@ -106,7 +131,7 @@ static void* _worker_fn(void *op) {
       if (tp->head == 0) {
         tp->tail = 0;
       }
-      --tp->cnt;
+      --tp->queue_size;
       free(h);
     }
     pthread_mutex_unlock(&tp->mtx);
@@ -116,6 +141,7 @@ static void* _worker_fn(void *op) {
     }
 
     pthread_mutex_lock(&tp->mtx);
+    --tp->num_threads_busy;
     if (tp->head) {
       pthread_mutex_unlock(&tp->mtx);
       continue;
@@ -123,41 +149,73 @@ static void* _worker_fn(void *op) {
       pthread_mutex_unlock(&tp->mtx);
       break;
     }
-    pthread_cond_wait(&tp->cond, &tp->mtx);
+    if (idx < tp->num_threads) {
+      pthread_cond_wait(&tp->cond, &tp->mtx);
+    } else { // Overflow thread will be terminated immediately.
+      iwulist_remove_first_by(&tp->threads, &st);
+      pthread_mutex_unlock(&tp->mtx);
+      break;
+    }
     pthread_mutex_unlock(&tp->mtx);
   }
   return 0;
 }
 
-iwrc iwtp_start(const char *thread_name_prefix, int num_threads, int queue_limit, IWTP *out_tp) {
-  if (num_threads < 1 || num_threads > 1023 || queue_limit < 0 || !out_tp) {
+iwrc iwtp_start_spec(const struct iwtp_spec *spec, IWTP *out_tp) {
+  iwrc rc = 0;
+  if (!spec || spec->num_threads < 1 || spec->num_threads > 1023 || spec->queue_limit < 0 || !out_tp) {
     return IW_ERROR_INVALID_ARGS;
   }
-  if (thread_name_prefix && strlen(thread_name_prefix) > 15) {
+  if (spec->thread_name_prefix && strlen(spec->thread_name_prefix) > 15) {
     return IW_ERROR_INVALID_ARGS;
   }
 
-  struct iwtp *tp = malloc(sizeof(*tp) + sizeof(pthread_t) * num_threads);
+  struct iwtp *tp = malloc(sizeof(*tp) + sizeof(pthread_t) * spec->num_threads);
   if (!tp) {
-    *out_tp = 0;
-    return iwrc_set_errno(IW_ERROR_ALLOC, errno);
+    rc = iwrc_set_errno(IW_ERROR_ALLOC, errno);
+    goto finish;
   }
+
   *tp = (struct iwtp) {
-    .num_threads = num_threads,
-    .queue_limit = queue_limit,
+    .warn_on_overflow_thread_spawn = spec->warn_on_overflow_thread_spawn,
+    .overflow_threads_factor = spec->overflow_threads_factor,
+    .num_threads = spec->num_threads,
+    .queue_limit = spec->queue_limit,
     .mtx = PTHREAD_MUTEX_INITIALIZER,
     .cond = PTHREAD_COND_INITIALIZER
   };
-  if (thread_name_prefix) {
-    tp->thread_name_prefix = strdup(thread_name_prefix);
+
+  if (spec->thread_name_prefix) {
+    tp->thread_name_prefix = strdup(spec->thread_name_prefix);
   }
-  tp->threads = (void*) ((char*) tp + sizeof(*tp));
-  memset(tp->threads, 0, sizeof(pthread_t) * num_threads);
-  while (num_threads--) {
-    pthread_create(&tp->threads[num_threads], 0, _worker_fn, tp);
+
+  iwulist_init(&tp->threads, spec->num_threads, sizeof(pthread_t));
+  for (size_t i = 0; i < spec->num_threads; ++i) {
+    pthread_t th;
+    int rci = pthread_create(&th, 0, _worker_fn, tp);
+    if (rci) {
+      rc = iwrc_set_errno(IW_ERROR_THREADING_ERRNO, rci);
+      iwlog_ecode_error3(rc);
+      iwtp_shutdown(&tp, false);
+      goto finish;
+    }
   }
-  *out_tp = tp;
-  return 0;
+
+finish:
+  if (!rc) {
+    *out_tp = tp;
+  } else {
+    *out_tp = 0;
+  }
+  return rc;
+}
+
+iwrc iwtp_start(const char *thread_name_prefix, int num_threads, int queue_limit, IWTP *out_tp) {
+  return iwtp_start_spec(&(struct iwtp_spec) {
+    .thread_name_prefix = thread_name_prefix,
+    .num_threads = num_threads,
+    .queue_limit = queue_limit
+  }, out_tp);
 }
 
 iwrc iwtp_shutdown(IWTP *tpp, bool wait_for_all) {
@@ -168,16 +226,16 @@ iwrc iwtp_shutdown(IWTP *tpp, bool wait_for_all) {
   if (!__sync_bool_compare_and_swap(&tp->shutdown, 0, 1)) {
     return 0;
   }
+  IWULIST *joinlist = 0;
 
   pthread_mutex_lock(&tp->mtx);
   pthread_t st = pthread_self();
-  for (int i = 0; i < tp->num_threads; ++i) {
-    if (tp->threads[i] == st) {
-      pthread_mutex_unlock(&tp->mtx);
-      iwlog_error("iwtp | Thread iwtp_shutdown() from one of pool thread: %lu", (unsigned long) st);
-      return IW_ERROR_ASSERTION;
-    }
+  if (iwulist_find_first(&tp->threads, &st) != -1) {
+    pthread_mutex_unlock(&tp->mtx);
+    iwlog_error("iwtp | Thread iwtp_shutdown() from one of pool thread: %lu", (unsigned long) st);
+    return IW_ERROR_ASSERTION;
   }
+
   if (!wait_for_all) {
     struct task *t = tp->head;
     while (t) {
@@ -187,19 +245,21 @@ iwrc iwtp_shutdown(IWTP *tpp, bool wait_for_all) {
     }
     tp->head = 0;
     tp->tail = 0;
-    tp->cnt = 0;
+    tp->queue_size = 0;
   }
+  joinlist = iwulist_clone(&tp->threads);
   pthread_cond_broadcast(&tp->cond);
   pthread_mutex_unlock(&tp->mtx);
 
-  for (int i = 0; i < tp->num_threads; ++i) {
-    if (tp->threads[i]) {
-      pthread_join(tp->threads[i], 0);
-    }
+  for (size_t i = 0, l = iwulist_length(joinlist); i < l; ++i) {
+    pthread_t jt = *(pthread_t*) iwulist_at2(joinlist, i);
+    pthread_join(jt, 0);
   }
 
   pthread_cond_destroy(&tp->cond);
   pthread_mutex_destroy(&tp->mtx);
+  iwulist_destroy_keep(&tp->threads);
+  iwulist_destroy(&joinlist);
   free(tp->thread_name_prefix);
   free(tp);
   *tpp = 0;
@@ -209,7 +269,7 @@ iwrc iwtp_shutdown(IWTP *tpp, bool wait_for_all) {
 int iwtp_queue_size(IWTP tp) {
   int res = 0;
   pthread_mutex_lock(&tp->mtx);
-  res = tp->cnt;
+  res = tp->queue_size;
   pthread_mutex_unlock(&tp->mtx);
   return res;
 }
--- src/utils/iwtp.h
+++ src/utils/iwtp.h
@@ -37,10 +37,40 @@ IW_EXTERN_C_START
 struct iwtp;
 typedef struct iwtp*IWTP;
 
+struct iwtp_spec {
+  /** Optional thread name prefix in thread pool.
+   * @note Thread name length must be not greater then 15 characters.
+   */
+  const char *thread_name_prefix;
+
+  /** Number of hot threads in thread pool.
+   * Threads are allocated on when thread pool created.
+   * Value must be in rage [1-1024]. */
+  int num_threads;
+
+  /** Maximum number of tasks in queue. Use zero for no limits. */
+  int queue_limit;
+
+  /** If task queue is full and the `overflow_threads_factor` is not zero
+   * then pool is allowed to spawn extra threads to process tasks as long
+   * as overall number of threads less of equal to `num_threads * overflow_threads_factor` */
+  int overflow_threads_factor;
+
+  /**
+   * It true performs log warning in the case of spawning overflow thread.
+   */
+  bool warn_on_overflow_thread_spawn;
+};
+
 /**
  * @brief Task to execute
  */
 typedef void (*iwtp_task_f)(void *arg);
+
+/**
+ * @brief Creates a new thread pool instance using provided `spec` config.
+ */
+IW_EXPORT iwrc iwtp_start_spec(const struct iwtp_spec *spec, IWTP *out_tp);
 
 /**
  * @brief Creates a new thread pool instance.
