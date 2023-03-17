ef4963a709e5bc7aac070756775d2b02425549d2
Anton Adamansky <adamansky@gmail.com>
2023-03-17 11:45:14 +0200

    * Added iwchars_is_digits()

--- src/utils/iwchars.h
+++ src/utils/iwchars.h
@@ -3,6 +3,7 @@
 #define IWCHARS_H
 
 #include "basedefs.h"
+#include <stdbool.h>
 
 IW_INLINE bool iwchars_is_blank(char c) {
   return c == 32 || c == 9;
@@ -22,6 +23,15 @@ IW_INLINE bool iwchars_is_alpha(char c) {
 
 IW_INLINE bool iwchars_is_alnum(char c) {
   return iwchars_is_alpha(c) || iwchars_is_digit(c);
+}
+
+static bool iwchars_is_digits(const char *str, int len) {
+  for (int i = 0; i < len; ++i) {
+    if (!iwchars_is_digit(str[i])) {
+      return false;
+    }
+  }
+  return true;
 }
 
 #endif
